57c8f8e7ec5fc0e05eefb63cc562adb19828ea6f
Josemar Luedke <josemarluedke@gmail.com>
2017-01-30 13:44:43 -0800

    Refactor getDOM to fix issue when running Ember in FastBoot with Glimmer

---
+++ addon/utils/document.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+let { getOwner } = Ember;
+
+// Private Ember API usage. Get the dom implementation used by the current
+// renderer, be it native browser DOM or Fastboot SimpleDOM
+export function getDocument(context) {
+  let container = getOwner ? getOwner(context) : context.container;
+  let documentService = container.lookup('service:-document');
+
+  if (documentService) { return documentService; }
+
+  let renderer = container.lookup('renderer:-dom');
+
+  if (renderer._dom && renderer._dom.document) { // pre Ember 2.6
+    return renderer._dom.document;
+  } else {
+    throw new Error('ember-mobiledoc-dom-renderer could not get DOM');
+  }
+}
