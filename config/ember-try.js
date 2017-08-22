b5a46fa1a5e64f8edb20395f8651a1b1576c72bd
Cory <cory.forsyth@gmail.com>
2017-08-22 13:48:41 -0400

    Add ember-2.12 to ember-try config

--- config/ember-try.js
+++ config/ember-try.js
@@ -47,6 +47,22 @@ module.exports = {
       }
     },
     {
+      name: 'ember-lts-2.12',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#2.12.2'
+        },
+        resolutions: {
+          'ember': '2.12.2'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
+        }
+      }
+    },
+    {
       name: 'ember-release',
       bower: {
         dependencies: {
